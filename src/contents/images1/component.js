7d64ff46ac4f7b0819085250993d7299dc887df8
huitou <3.riverside.swe@gmail.com>
2020-08-11 18:50:49 +0100

    n.9 - add viewport visibility and deferred img loading (#10)
    * n.9 - add viewport visibility and deferred img loading
    
    * n.9 - use new images

--- src/contents/images1/component.js
+++ src/contents/images1/component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 import {
   imagesArea,
@@ -11,26 +12,58 @@ import {
 } from './component.css';
 
 class Images1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isImage1Visible: false,
+      isImage2Visible: false,
+    };
+
+    this.onVisibility1Change = this.onVisibility1Change.bind(this);
+    this.onVisibility2Change = this.onVisibility2Change.bind(this);
+  }
+
+  onVisibility1Change(isVisible) {
+    isVisible && setTimeout(
+      () => this.setState(({ isImage1Visible }) => !isImage1Visible ? { isImage1Visible: true } : {}),
+      1000
+    );
+  }
+  onVisibility2Change(isVisible) {
+    isVisible && setTimeout(
+      () => this.setState(({ isImage2Visible }) => !isImage2Visible ? { isImage2Visible: true } : {}),
+      1500
+    );
+  }
+
   render() {
+    const { isImage1Visible, isImage2Visible} = this.state;
+
     return (
       <div className={imagesArea}>
         <div className={imageArea1}>
-          <div className={imageFrame1}>
-            {/* <img
-              className={image1}
-              src="/images/image-1.1.png"
-              alt="Food Sharing App - Leftlovers"
-            /> */}
-          </div>
+          <VisibilitySensor onChange={this.onVisibility1Change}>
+            <div className={imageFrame1}>
+              {isImage1Visible && <img
+                className={image1}
+                src="/images/image-1.1.png"
+                alt="Food Sharing App - Leftlovers"
+              />
+              }
+            </div>
+          </VisibilitySensor>
         </div>
         <div className={imageArea2}>
-        <div className={imageFrame2}>
-            {/* <img
-              className={image2}
-              src="/images/image-1.2.png"
-              alt="Education Platform"
-            /> */}
-          </div>
+          <VisibilitySensor onChange={this.onVisibility2Change}>
+            <div className={imageFrame2}>
+              {isImage2Visible && <img
+                className={image2}
+                src="/images/image-1.2.png"
+                alt="Education Platform"
+              />
+              }
+            </div>
+          </VisibilitySensor>
         </div>
       </div>
     );
