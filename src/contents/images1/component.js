2f2b84e530adb17ce76f85892a35c84dfae25797
huitou <3.riverside.swe@gmail.com>
2020-08-11 19:31:03 +0100

    n.11 - add hover on image frames

--- src/contents/images1/component.js
+++ src/contents/images1/component.js
@@ -17,10 +17,16 @@ class Images1 extends Component {
     this.state = {
       isImage1Visible: false,
       isImage2Visible: false,
+      hoverOnImageFrame1: false,
+      hoverOnImageFrame2: false,
     };
 
     this.onVisibility1Change = this.onVisibility1Change.bind(this);
     this.onVisibility2Change = this.onVisibility2Change.bind(this);
+    this.onMouseEnterImageFrame1 = this.onMouseEnterImageFrame1.bind(this);
+    this.onMouseEnterImageFrame2 = this.onMouseEnterImageFrame2.bind(this);
+    this.onMouseLeaveImageFrame1 = this.onMouseLeaveImageFrame1.bind(this);
+    this.onMouseLeaveImageFrame2 = this.onMouseLeaveImageFrame2.bind(this);
   }
 
   onVisibility1Change(isVisible) {
@@ -36,32 +42,61 @@ class Images1 extends Component {
     );
   }
 
+  onMouseEnterImageFrame1() {
+    this.setState({ hoverOnImageFrame1: true });
+  }
+  onMouseEnterImageFrame2() {
+    this.setState({ hoverOnImageFrame2: true });
+  }
+
+  onMouseLeaveImageFrame1() {
+    this.setState({ hoverOnImageFrame1: false });
+  }
+  onMouseLeaveImageFrame2() {
+    this.setState({ hoverOnImageFrame2: false });
+  }
+
   render() {
-    const { isImage1Visible, isImage2Visible} = this.state;
+    const {
+      isImage1Visible,
+      isImage2Visible,
+      hoverOnImageFrame1,
+      hoverOnImageFrame2,
+    } = this.state;
 
     return (
       <div className={imagesArea}>
         <div className={imageArea1}>
           <VisibilitySensor onChange={this.onVisibility1Change}>
-            <div className={imageFrame1}>
-              {isImage1Visible && <img
-                className={image1}
-                src="/images/image-1.1.png"
-                alt="Food Sharing App - Leftlovers"
-              />
-              }
+            <div className={imageFrame1}
+              onMouseEnter={this.onMouseEnterImageFrame1}
+              onMouseLeave={this.onMouseLeaveImageFrame1}
+            >
+              {isImage1Visible && (hoverOnImageFrame1
+                ? null
+                : <img
+                  className={image1}
+                  src="/images/image-1.1.png"
+                  alt="Food Sharing App - Leftlovers"
+                />
+              )}
             </div>
           </VisibilitySensor>
         </div>
         <div className={imageArea2}>
           <VisibilitySensor onChange={this.onVisibility2Change}>
-            <div className={imageFrame2}>
-              {isImage2Visible && <img
-                className={image2}
-                src="/images/image-1.2.png"
-                alt="Education Platform"
-              />
-              }
+            <div className={imageFrame2}
+              onMouseEnter={this.onMouseEnterImageFrame2}
+              onMouseLeave={this.onMouseLeaveImageFrame2}
+            >
+              {isImage2Visible && (hoverOnImageFrame2
+                ? null
+                : <img
+                  className={image2}
+                  src="/images/image-1.2.png"
+                  alt="Education Platform"
+                />
+              )}
             </div>
           </VisibilitySensor>
         </div>
--- src/contents/images2/component.js
+++ src/contents/images2/component.js
@@ -21,6 +21,10 @@ class Images2 extends Component {
 
     this.onVisibility1Change = this.onVisibility1Change.bind(this);
     this.onVisibility2Change = this.onVisibility2Change.bind(this);
+    this.onMouseEnterImageFrame1 = this.onMouseEnterImageFrame1.bind(this);
+    this.onMouseEnterImageFrame2 = this.onMouseEnterImageFrame2.bind(this);
+    this.onMouseLeaveImageFrame1 = this.onMouseLeaveImageFrame1.bind(this);
+    this.onMouseLeaveImageFrame2 = this.onMouseLeaveImageFrame2.bind(this);
   }
 
   onVisibility1Change(isVisible) {
@@ -36,32 +40,61 @@ class Images2 extends Component {
     );
   }
 
+  onMouseEnterImageFrame1() {
+    this.setState({ hoverOnImageFrame1: true });
+  }
+  onMouseEnterImageFrame2() {
+    this.setState({ hoverOnImageFrame2: true });
+  }
+
+  onMouseLeaveImageFrame1() {
+    this.setState({ hoverOnImageFrame1: false });
+  }
+  onMouseLeaveImageFrame2() {
+    this.setState({ hoverOnImageFrame2: false });
+  }
+
   render() {
-    const { isImage1Visible, isImage2Visible} = this.state;
+    const {
+      isImage1Visible,
+      isImage2Visible,
+      hoverOnImageFrame1,
+      hoverOnImageFrame2,
+    } = this.state;
 
     return (
       <div className={imagesArea}>
         <div className={imageArea1}>
           <VisibilitySensor onChange={this.onVisibility1Change}>
-            <div className={imageFrame1}>
-              {isImage1Visible && <img
-                className={image1}
-                src="/images/image-2.1.png"
-                alt="Music Stream Platform"
-              />
-              }
+            <div className={imageFrame1}
+              onMouseEnter={this.onMouseEnterImageFrame1}
+              onMouseLeave={this.onMouseLeaveImageFrame1}
+            >
+              {isImage1Visible && (hoverOnImageFrame1
+                ? null
+                : <img
+                  className={image1}
+                  src="/images/image-2.1.png"
+                  alt="Music Stream Platform"
+                />
+              )}
             </div>
           </VisibilitySensor>
         </div>
         <div className={imageArea2}>
           <VisibilitySensor onChange={this.onVisibility2Change}>
-            <div className={imageFrame2}>
-              {isImage2Visible && <img
-                className={image2}
-                src="/images/image-2.2.png"
-                alt="eCommerce Website"
-              />
-              }
+            <div className={imageFrame2}
+              onMouseEnter={this.onMouseEnterImageFrame2}
+              onMouseLeave={this.onMouseLeaveImageFrame2}
+            >
+              {isImage2Visible && (hoverOnImageFrame2
+                ? null
+                : <img
+                  className={image2}
+                  src="/images/image-2.2.png"
+                  alt="eCommerce Website"
+                />
+              )}
             </div>
           </VisibilitySensor>
         </div>
