5c5041e9266a4f581dad5fc7759c62773b2e80f9
Fuhang Wang <fuhang@CeXs-MacBook-Pro.local>
2020-08-17 16:42:53 +0100

    n.29 - add routing function - step 1

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import MainPage from './pages/main';
-import Work1Page from './pages/work-1';
+import Work3Page from './pages/work-3';
 
 import './app.css';
 
 class App extends Component {
   render() {
     return (
-      // <MainPage />
-      <Work1Page />
+      <Router>
+        <Switch>
+          <Route path="/work-1">
+            <Work3Page />
+          </Route>
+          <Route path="/work-2">
+            <Work3Page />
+          </Route>
+          <Route path="/work-3">
+            <Work3Page />
+          </Route>
+          <Route path="/work-4">
+            <Work3Page />
+          </Route>
+          <Route path="/">
+            <MainPage />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
--- src/pages/work-1/index.js
+++
@@ -1,3 +0,0 @@
-import Work1Page from './component';
-
-export default Work1Page;
