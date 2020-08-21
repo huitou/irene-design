4d7f4372957be5aecd71986389e8cb1869fd28cc
Fuhang Wang <fuhang@CeXs-MacBook-Pro.local>
2020-08-21 20:39:44 +0100

    n.17 - refine work page

--- src/contents/learnings/component.js
+++ src/contents/learnings/component.js
@@ -75,16 +75,16 @@ class Learnings extends Component {
     return (
       <div className={introductionArea}>
         <div className={introductionFrame}>
-          {resources[0] && (
+          {(resources[0] || resources[1]) && (
             <Fragment>
-              <div key={resources[0].id} className={imageArea}>
+              {resources[0] && <div key={resources[0].id} className={imageArea}>
                 <div className={imageFrame}>
                   <img className={image}
                     src={resources[0].imageSource.src}
                     alt={resources[0].imageSource.alt}
                   />
                 </div>
-              </div>
+              </div>}
               <div className={textFrame}>
                 <h4 className={title}>{titleText}</h4>
                 <p className={paragraph}>{resources[1].text}</p>
